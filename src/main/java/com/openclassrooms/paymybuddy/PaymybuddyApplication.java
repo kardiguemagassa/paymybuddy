f22e24b4f433364bce25cfcbc281c4f38236dac9
kardiguemagassa <129184661+kardiguemagassa@users.noreply.github.com>
2025-03-12 18:29:20 +0100

    Refacto those files

--- src/main/java/com/openclassrooms/paymybuddy/PaymybuddyApplication.java
+++ src/main/java/com/openclassrooms/paymybuddy/PaymybuddyApplication.java
@@ -3,11 +3,24 @@ package com.openclassrooms.paymybuddy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 @SpringBootApplication
 public class PaymybuddyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymybuddyApplication.class, args);
+
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println(now);
+
+		LocalDateTime localDateTime = LocalDateTime.now();
+		Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+		System.out.println(date);
+
+
 	}
 
 }
