b8b8a45b5dc30affbb62f1c818c3fe0322d30284
kardiguemagassa <129184661+kardiguemagassa@users.noreply.github.com>
2025-04-08 12:13:54 +0200

    UserServiceImplTest End unit test

--- src/test/java/com/openclassrooms/paymybuddy/service/UserServiceImplTest.java
+++ src/test/java/com/openclassrooms/paymybuddy/service/UserServiceImplTest.java
@@ -1,6 +1,9 @@
 package com.openclassrooms.paymybuddy.service;
 
 import com.openclassrooms.paymybuddy.enttity.User;
+import com.openclassrooms.paymybuddy.exception.EmailExistsException;
+import com.openclassrooms.paymybuddy.exception.InvalidPasswordException;
+import com.openclassrooms.paymybuddy.exception.PasswordMismatchException;
 import com.openclassrooms.paymybuddy.exception.UserNotFoundException;
 import com.openclassrooms.paymybuddy.repository.UserRepository;
 import com.openclassrooms.paymybuddy.service.serviceImpl.SecurityValidationImpl;
@@ -19,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -26,7 +30,7 @@ import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -64,14 +68,14 @@ public class UserServiceImplTest {
 
     @Test
     void registerUser_shouldEncodePasswordAndSaveUser() {
-        LOGGER.debug("Starting test: registerUser_shouldEncodePasswordAndSaveUser");
+        LOGGER.info("Starting test: registerUser_shouldEncodePasswordAndSaveUser");
 
         // Given
         String plainPassword = "plainPassword";
         String encodedPassword = "encodedTestPassword";
         testUser.setPassword(plainPassword);
 
-        LOGGER.debug("Plain password before encoding: {}", plainPassword);
+        LOGGER.info("Plain password before encoding: {}", plainPassword);
         when(passwordEncoder.encode(plainPassword)).thenReturn(encodedPassword);
 
         // When
@@ -82,8 +86,8 @@ public class UserServiceImplTest {
         verify(userRepository).save(testUser);
         assertEquals(encodedPassword, testUser.getPassword());
 
-        LOGGER.debug("Encoded password after processing: {}", testUser.getPassword());
-        LOGGER.debug("Test completed successfully");
+        LOGGER.info("Encoded password after processing: {}", testUser.getPassword());
+        LOGGER.info("Test completed successfully");
     }
 
     @Test
@@ -103,91 +107,233 @@ public class UserServiceImplTest {
     @Test
     void getUserByEmail_whenUserExists_shouldReturnUser() {
         // given
-        LOGGER.debug("Starting test: getUserByEmail_shouldReturnUser");
-        when(userRepository.findByEmail("john@email.com")).thenReturn(Optional.of(testUser));
-        User user = userService.getUserByEmail("john@email.com");
+        LOGGER.info("Starting test: getUserByEmail_shouldReturnUser");
+        when(userRepository.findByEmail(testUser.getEmail())).thenReturn(Optional.of(testUser));
+        User user = userService.getUserByEmail(testUser.getEmail());
         assertEquals(testUser.getId(), user.getId());
     }
 
     @Test
     void getUserByEmail_whenUserNotExists_shouldThrowException() {
         // given
-        LOGGER.debug("Starting test: getUserByEmail_shouldThrowException");
-        when(userRepository.findByEmail("userNotExist@example.com")).thenReturn(Optional.empty());
+        LOGGER.info("Starting test: getUserByEmail_shouldThrowException");
+        when(userRepository.findByEmail("userNotExist@gmail.com")).thenReturn(Optional.empty());
         // when then
-        assertThrows(UserNotFoundException.class, () -> userService.getUserByEmail("userNotExist@example.com"));
+        assertThrows(UserNotFoundException.class, () -> userService.getUserByEmail("userNotExist@gmail.com"));
     }
 
-//    @Test
-//    void updateUser_shouldUpdateUsernameAndEmail() {
-//        // Given
-//        User existingUser = new User();
-//        existingUser.setId(1L);
-//        existingUser.setName("Old Name");
-//        existingUser.setEmail("old@email.com");
-//
-//        User updatedUser = new User();
-//        updatedUser.setName("New Name");
-//        updatedUser.setEmail("new@email.com");
-//
-//        when(userRepository.findById(1L)).thenReturn(Optional.of(existingUser));
-//        when(userRepository.save(any(User.class))).thenReturn(existingUser);
-//
-//        // Configurez le mock pour securityValidationImpl
-//        when(securityValidationImpl.validateEmail("new@email.com")).thenReturn(true);
-//        when(securityValidationImpl.validateEmailFormat("new@email.com")).thenReturn(true);
-//
-//        // When
-//        User result = userService.updateUser(1L, updatedUser);
-//
-//        // Then
-//        assertThat(result.getName()).isEqualTo("New Name");
-//        assertThat(result.getEmail()).isEqualTo("new@email.com");
-//    }
+    @Test
+    void findUserOptionalByEmail_whenUserExists_shouldReturnUser() {
+        LOGGER.info("Starting test: findUserOptionalByEmail_shouldReturnUser");
+        when(userRepository.findByEmail(testUser.getEmail())).thenReturn(Optional.of(testUser));
+        Optional<User> user = userService.findUserByEmail(testUser.getEmail());
+        assertEquals(testUser.getId(), user.orElse(null).getId());
+    }
 
-//    @Test
-//    void updateUser_shouldUpdateUsernameAndEmail() throws Exception {
-//        // given
-//        LOGGER.debug("Starting test: updateUser_shouldUpdateUsernameAndEmail");
-//
-//        // Configuration du répertoire de test
-//        Path uploadPath = Paths.get("target/test-uploads");
-//        if (!Files.exists(uploadPath)) {
-//            Files.createDirectories(uploadPath);
-//        }
-//
-//        // Injection directe sans Spring
-//        Field uploadDirField = UserServiceImpl.class.getDeclaredField("uploadDir");
-//        uploadDirField.setAccessible(true);
-//        uploadDirField.set(userService, uploadPath.toString());
-//
-//        User existingUser = new User();
-//        existingUser.setEmail("john@email.com");
-//        existingUser.setName("John");
-//        existingUser.setPassword("encodedPassword");
-//
-//        when(userRepository.findByEmail("john@email.com")).thenReturn(Optional.of(existingUser));
-//        when(userRepository.findByEmail("jane@gmail.com")).thenReturn(Optional.empty());
-//        when(userRepository.save(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
-//
-//        MultipartFile mockFile = mock(MultipartFile.class);
-//        when(mockFile.isEmpty()).thenReturn(false);
-//        when(mockFile.getOriginalFilename()).thenReturn("profile.jpg");
-//        when(mockFile.getInputStream()).thenReturn(new ByteArrayInputStream("test".getBytes()));
-//
-//        // when
-//        User updatedUser = userService.updateUser("john@email.com", "newUsername", "jane@gmail.com", mockFile);
-//
-//        // then
-//        assertNotNull(updatedUser);
-//        assertEquals("newUsername", updatedUser.getName());
-//        assertEquals("jane@gmail.com", updatedUser.getEmail());
-//        assertNotNull(updatedUser.getProfileImageUrl());
-//
-//        // Nettoyage
-//        Files.walk(uploadPath)
-//                .sorted(Comparator.reverseOrder())
-//                .map(Path::toFile)
-//                .forEach(File::delete);
-//    }
+    @Test
+    void updateUser_shouldUpdateUsernameAndEmail() throws Exception {
+        // given
+        LOGGER.info("Starting test: updateUser_shouldUpdateUsernameAndEmail");
+
+        // Configuration du répertoire de test
+        Path uploadPath = Paths.get("target/test-uploads");
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        // Injection directe sans Spring
+        Field uploadDirField = UserServiceImpl.class.getDeclaredField("uploadDir");
+        uploadDirField.setAccessible(true);
+        uploadDirField.set(userService, uploadPath.toString());
+
+        User existingUser = new User();
+        existingUser.setEmail("john@email.com");
+        existingUser.setName("John");
+        existingUser.setPassword("encodedPassword");
+
+        when(userRepository.findByEmail("john@email.com")).thenReturn(Optional.of(existingUser));
+        when(userRepository.findByEmail("jane@gmail.com")).thenReturn(Optional.empty());
+        when(userRepository.save(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        MultipartFile mockFile = mock(MultipartFile.class);
+        when(mockFile.isEmpty()).thenReturn(false);
+        when(mockFile.getOriginalFilename()).thenReturn("profile.jpg");
+        when(mockFile.getInputStream()).thenReturn(new ByteArrayInputStream("test".getBytes()));
+
+        // when
+        User updatedUser = userService.updateUser("john@email.com", "newUsername", "jane@gmail.com", mockFile);
+
+        // then
+        assertNotNull(updatedUser);
+        assertEquals("newUsername", updatedUser.getName());
+        assertEquals("jane@gmail.com", updatedUser.getEmail());
+        assertNotNull(updatedUser.getProfileImageUrl());
+
+        // Nettoyage
+        Files.walk(uploadPath)
+                .sorted(Comparator.reverseOrder())
+                .map(Path::toFile)
+                .forEach(File::delete);
+    }
+
+    @Test
+    void  updateUser_whenNewEmailAlreadyExists_shouldThrowEmailExistsException() {
+        LOGGER.info("Starting test: updateUser_whenNewEmailAlreadyExists_shouldThrowEmailExistsException");
+
+        // Given
+        String currentEmail = "john@email.com";
+        String newEmail = "john@gmail.com";
+        String newUsername = "newUsername";
+
+        User existingUser = new User();
+        existingUser.setEmail(currentEmail);
+        User anotherUser = new User();
+        anotherUser.setEmail(newEmail);
+
+        when(userRepository.findByEmail(currentEmail)).thenReturn(Optional.of(existingUser));
+        when(userRepository.findByEmail(newEmail)).thenReturn(Optional.of(anotherUser));
+
+        // When & Then
+        assertThrows(EmailExistsException.class, () -> userService.updateUser(currentEmail,newUsername,newEmail,null));
+        verify(userRepository, never()).save(any(User.class));
+    }
+
+    @Test
+    void updateUser_whenImageStorageFails_shouldThrowIOException() throws Exception {
+        LOGGER.info("Starting test: updateUser_whenImageStorageFails_shouldThrowIOException");
+
+        // Configuration du répertoire de test
+        Path uploadPath = Paths.get("target/test-uploads");
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        // Injection sans Spring
+        Field uploadDirField = UserServiceImpl.class.getDeclaredField("uploadDir");
+        uploadDirField.setAccessible(true);
+        uploadDirField.set(userService, uploadPath.toString());
+
+        // Given
+        String currentEmail = "john@gmail.com";
+        String newEmail = "new@gmail.com";
+        String newUsername = "newUsername";
+
+        User existingUser = new User();
+        existingUser.setEmail(currentEmail);
+
+        MultipartFile mockFile = mock(MultipartFile.class);
+        when(mockFile.isEmpty()).thenReturn(false);
+        when(mockFile.getOriginalFilename()).thenReturn("profile.jpg");
+        when(mockFile.getInputStream()).thenThrow(new IOException("Simulated storage error"));
+
+        when(userRepository.findByEmail(currentEmail)).thenReturn(Optional.of(existingUser));
+        when(userRepository.findByEmail(newEmail)).thenReturn(Optional.empty());
+
+        // When & Then
+        assertThrows(IOException.class, () ->
+                userService.updateUser(currentEmail, newUsername, newEmail, mockFile));
+
+        //  user n'a pas été modifié
+        verify(userRepository, never()).save(any());
+
+        // Nettoyage
+        Files.walk(uploadPath)
+                .sorted(Comparator.reverseOrder())
+                .map(Path::toFile)
+                .forEach(File::delete);
+    }
+
+        @Test
+    void updatePassword_whenCurrentPasswordIsCorrect_shouldUpdatePassword() throws PasswordMismatchException, InvalidPasswordException {
+        LOGGER.info("Starting test: updatePassword_whenCurrentPasswordIsCorrect_shouldUpdatePassword");
+
+        // Given
+        String currentPassword = "correctPassword";
+        String newPassword = "newSecurePassword123";
+        String confirmPassword = "newSecurePassword123";
+
+        when(userRepository.findByEmail(testUser.getEmail())).thenReturn(Optional.of(testUser));
+        when(passwordEncoder.matches(currentPassword, testUser.getPassword())).thenReturn(true);
+        when(passwordEncoder.matches(newPassword, testUser.getPassword())).thenReturn(false);
+        when(passwordEncoder.encode(newPassword)).thenReturn("newEncodedPassword");
+
+        // When
+        userService.updatePassword(testUser.getEmail(), currentPassword, newPassword, confirmPassword);
+
+        // Then
+        verify(userRepository).save(testUser);
+        assertEquals("newEncodedPassword", testUser.getPassword());
+    }
+
+    @Test
+    void updatePassword_whenUserNotFound_shouldThrowException() {
+        LOGGER.info("Starting test: updatePassword_whenUserNotFound_shouldThrowException");
+
+        // Given
+        String email = "userNotExist@gmail.com";
+        when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+
+        // When & Then
+        assertThrows(UserNotFoundException.class, () ->
+                userService.updatePassword(email, "any", "newPass", "newPass"));
+    }
+
+    @Test
+    void updatePassword_whenCurrentPasswordIncorrect_shouldThrowException() {
+        LOGGER.info("Starting test: updatePassword_whenCurrentPasswordIncorrect_shouldThrowException");
+
+        // Given
+        when(userRepository.findByEmail(testUser.getEmail())).thenReturn(Optional.of(testUser));
+        when(passwordEncoder.matches("wrongPassword", testUser.getPassword())).thenReturn(false);
+
+        // When & Then
+        assertThrows(InvalidPasswordException.class, () ->
+                userService.updatePassword(testUser.getEmail(), "wrongPassword", "newPass", "newPass"));
+    }
+
+    @Test
+    void updatePassword_whenPasswordsDontMatch_shouldThrowException() {
+        LOGGER.info("Starting test: updatePassword_whenPasswordsDontMatch_shouldThrowException");
+
+        // Given
+        when(userRepository.findByEmail(testUser.getEmail())).thenReturn(Optional.of(testUser));
+        when(passwordEncoder.matches("correctPassword", testUser.getPassword())).thenReturn(true);
+
+        // When & Then
+        assertThrows(PasswordMismatchException.class, () ->
+                userService.updatePassword(testUser.getEmail(), "correctPassword", "newPass", "differentPass"));
+    }
+
+    @Test
+    void updatePassword_whenNewPasswordSameAsCurrent_shouldThrowException() {
+        LOGGER.info("Starting test: updatePassword_whenNewPasswordSameAsCurrent_shouldThrowException");
+
+        // Given
+        String currentPassword = "correctPassword";
+        when(userRepository.findByEmail(testUser.getEmail())).thenReturn(Optional.of(testUser));
+        when(passwordEncoder.matches(currentPassword, testUser.getPassword())).thenReturn(true);
+        when(passwordEncoder.matches(currentPassword, testUser.getPassword())).thenReturn(true);
+
+        // When & Then
+        assertThrows(InvalidPasswordException.class, () ->
+                userService.updatePassword(testUser.getEmail(), currentPassword, currentPassword, currentPassword));
+    }
+
+    @Test
+    void updatePassword_whenNewPasswordInvalid_shouldThrowException() throws InvalidPasswordException {
+        LOGGER.info("Starting test: updatePassword_whenNewPasswordInvalid_shouldThrowException");
+
+        // Given
+        String currentPassword = "correctPassword";
+        String invalidPassword = "weakPassword";
+
+        when(userRepository.findByEmail(testUser.getEmail())).thenReturn(Optional.of(testUser));
+        when(passwordEncoder.matches(currentPassword, testUser.getPassword())).thenReturn(true);
+        doThrow(new InvalidPasswordException("Password too weakPassword"))
+                .when(securityValidationImpl).validatePassword(invalidPassword);
+
+        // When & Then
+        assertThrows(InvalidPasswordException.class, () ->
+                userService.updatePassword(testUser.getEmail(), currentPassword, invalidPassword, invalidPassword));
+    }
 }
